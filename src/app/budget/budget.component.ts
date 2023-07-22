375b7570547fa653e95890ae3480dbe32a4dfcee
Eoin <eoinconneely@gmail.com>
2023-07-22 17:02:58 +0100

    continuation of build

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,30 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ExpenseEntry, IncomeEntry } from '../shared/budget-entry.model';
 import { BudgetDataService } from '../shared/budget-data.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
   styleUrls: ['./budget.component.css']
 })
-export class BudgetComponent implements OnInit {
+export class BudgetComponent implements OnInit, OnDestroy {
 
   incomeEntries: IncomeEntry[] = [];
+  incomeSubscription: Subscription = new Subscription();
   expenseEntries: ExpenseEntry[] = [];
+  expenseSubscription: Subscription = new Subscription();
 
-  constructor (private budgetDataService: BudgetDataService) { }
+  constructor(private budgetDataService: BudgetDataService) { }
 
-  ngOnInit () : void {
-  this.incomeEntries = this.budgetDataService.incomeEntries;
-  this.expenseEntries = this.budgetDataService.expenseEntries;
-  }
+  ngOnInit(): void {
+    this.incomeSubscription = this.budgetDataService.incomeSubject.subscribe(incomeEntries => {
+      this.incomeEntries = incomeEntries;
+    });
 
-  incomeDelete ( index: number){
-    this.budgetDataService.incomeDelete (index) ;
+    this.expenseSubscription = this.budgetDataService.expenseSubject.subscribe(expenseEntries => {
+      this.expenseEntries = expenseEntries;
+    });
+
+    this.expenseEntries = this.budgetDataService.expenseEntries;
     this.incomeEntries = this.budgetDataService.incomeEntries;
   }
-  expenseDelete ( index: number){
-    this.budgetDataService.expenseDelete (index) ;
-    this.expenseEntries = this.budgetDataService.expenseEntries;
+
+  ngOnDestroy(): void {
+    this.incomeSubscription.unsubscribe();
+    this.expenseSubscription.unsubscribe();
+  }
+
+  incomeDelete(index: number) {
+    this.budgetDataService.incomeDelete(index);
+  }
+
+  expenseDelete(index: number) {
+    this.budgetDataService.expenseDelete(index);
   }
 }
--- src/app/shared/budget-data.component.ts
+++ src/app/shared/budget-data.component.ts
@@ -1,19 +1,31 @@
 import { Injectable } from "@angular/core";
 import { ExpenseEntry, IncomeEntry } from "./budget-entry.model";
+import { Subject } from "rxjs";
 
+@Injectable({ providedIn: "root" })
+export class BudgetDataService {
 
-@Injectable({providedIn: "root" })
-export class BudgetDataService{
+  incomeSubject = new Subject<IncomeEntry[]>();
+  expenseSubject = new Subject<ExpenseEntry[]>();
 
-    incomeEntries: IncomeEntry [] = [
-        new IncomeEntry ("500", "50", "Wages"),
-        
+  incomeEntries: IncomeEntry[] = [
+    new IncomeEntry("500", "50", "Wages"),
+    // Add more initial income entries if needed
+  ];
 
-    ]
-    expenseEntries: ExpenseEntry [] = [
-        new ExpenseEntry ("40", "50", "Phone bill"),
-        new ExpenseEntry ("40", "50", "Groceries"),
-        
+  expenseEntries: ExpenseEntry[] = [
+    new ExpenseEntry("40", "50", "Phone bill"),
+    new ExpenseEntry("40", "50", "Groceries"),
+    // Add more initial expense entries if needed
+  ];
 
-    ]
+  incomeDelete(index: number) {
+    this.incomeEntries.splice(index, 1);
+    this.incomeSubject.next(this.incomeEntries);
+  }
+
+  expenseDelete(index: number) {
+    this.expenseEntries.splice(index, 1);
+    this.expenseSubject.next(this.expenseEntries);
+  }
 }
